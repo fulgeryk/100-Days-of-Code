54a41ec8f1b9c31663d18b2b8337cf002b5a5b7f
Fulger Sorin <fulgeryk_cs@yahoo.com>
2025-09-15 19:39:05 +0300

    Save high score in a file

---
+++ Day24/main.py
@@ -0,0 +1,45 @@
+from turtle import Screen
+from Snake import Snake
+from food import Food
+from scoreborad import Scoreboard
+import time
+
+screen = Screen()
+screen.setup(width=600,height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+score = Scoreboard()
+screen.listen()
+
+screen.onkey(key= "Up" , fun= snake.up)
+screen.onkey(key= "Left" , fun= snake.left)
+screen.onkey(key= "Right" , fun= snake.right)
+screen.onkey(key= "Down" , fun= snake.down)
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+    #Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        score.increase_score()
+
+    #Detect collision with xcor and ycor
+    if snake.head.xcor() > 280 or snake.head.xcor() < - 280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        score.reset()
+        snake.reset()
+
+    #Detect collision with tail
+    for segment in snake.snake[1:]:
+        if snake.head.distance(segment) < 10:
+            score.reset()
+            snake.reset()
+
+screen.exitonclick()
